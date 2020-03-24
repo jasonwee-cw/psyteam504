14e4acf764b3ae7f721a2a996dddad1584fedc3f
leonm8 <60977048+leonm8@users.noreply.github.com>
2020-03-24 15:09:40 -0400

    Update stan_fullModels.R

--- stan_fullModels.R
+++ stan_fullModels.R
@@ -1,10 +1,20 @@
+### PSY504 Psych Team
+### Crystal Lee & Leon Mait
+### Created: 03/13/20
+### Modified: 03/24/20
+### Last edited by: Leon 
+
 ### Load packages 
 library(tidyr)
 library(dplyr)
 library(rstan)
+options(mc.cores = parallel::detectCores())
+rstan_options(auto_write = TRUE)
 
 ## Set wd
-setwd("~/myrepos/psyteam504")
+setwd("~/Documents/Princeton/PSY504 Statistics")
+
+#setwd("~/myrepos/psyteam504")
 
 ###################################
 ##### Mixture model ###############
@@ -13,57 +23,56 @@ setwd("~/myrepos/psyteam504")
 ## Load file
 lotto <- read.csv("lotteriesOvert.csv")
 nrow(lotto) #1507
-'''
-R = Proportion of risky (= higher variance) choices
-H = Proportion of higher expected value choices (EV = probability of payoff (%) * payoff amount ($))
-CV = Proportion of higher coefficient of variance choices (CoV = the dispersion of data points in a data series around the mean)
-'''
+
+# R = Proportion of risky (= higher variance) choices
+# H = Proportion of higher expected value choices (EV = probability of payoff (%) * payoff amount ($))
+# CV = Proportion of higher coefficient of variance choices (CoV = the dispersion of data points in a data series around the mean)
 
 ## Set up parameters
 y <- lotto$R # DV
 K <- 2 # No of groups
 N <- as.numeric(nrow(lotto))
 
-### Parse into list for Stan
+## Parse into list for Stan
 lotteries_data <- list(K=K, N=N, y=y)
-
 
 ## Specify Model
 multi_text <-
   "data {
-  int<lower=1> K;          // number of mixture components
-  int<lower=1> N;          // number of data points
-  real y[N];               // observations
-  
-}
-parameters {
-  simplex[K] theta;          // mixing proportions
-  ordered[K] mu;             // locations of mixture components
-  vector<lower=0>[K] sigma;  // scales of mixture components
-}
-model {
-  vector[K] log_theta = log(theta);  // cache log calculation
-  sigma ~ lognormal(0, 2);
-  mu ~ normal(0, 10);
-  for (n in 1:N) {
+    int<lower=1> K;          // number of mixture components
+    int<lower=1> N;          // number of data points
+    real y[N];               // observations
+  }
+
+  parameters {
+    simplex[K] theta;          // mixing proportions
+    ordered[K] mu;             // locations of mixture components
+    vector<lower=0>[K] sigma;  // scales of mixture components
+    }
+
+  model {
+    vector[K] log_theta = log(theta);  // cache log calculation
+    sigma ~ lognormal(0, 2);
+    mu ~ normal(0, 1);
+    for (n in 1:N) {
     vector[K] lps = log_theta;
     for (k in 1:K)
-      lps[k] += normal_lpdf(y[n] | mu[k], sigma[k]);
+    lps[k] += normal_lpdf(y[n] | mu[k], sigma[k]);
     target += log_sum_exp(lps);
+    }
   }
-  
-}
 "
 
-# Fit Model
+## Fit Model
 lotteries_multi_fit <- stan(model_code=multi_text, data = lotteries_data,
                             verbose=TRUE, chains = 1)
 
-summary.lottery <- summary(lotteries_multi_fit, c("theta"))
+summary.lottery <- summary(lotteries_multi_fit)
 
-# Calculating posterior cluster probabilities per participant
-mu1 <- as.data.frame(summary.lottery$summary)$mean[1]
-sd1 <- as.data.frame(summary.lottery$summary)$sd[1]
+## Calculate posterior cluster probabilities per participant
+theta1 <- as.data.frame(summary.lottery$summary)$mean[1]
+mu1 <- as.data.frame(summary.lottery$summary)$mean[3]
+sd1 <- as.data.frame(summary.lottery$summary)$mean[5]
 
 lotto %>%
   group_by(partid) %>%
@@ -83,13 +92,9 @@ lotto %>%
 # lotto.sum <- summary(lotteries_multi_fit)
 # lotto.sum$summary
 
-
-
-### This is older stuff below
-
 # Gathering data
 bart_data <- read.csv("bart_pumps.csv", header = TRUE)
-bart_subset <- top_n(bart_data, 1000) #this used to say df.bart instead of bart_data, so I [Leon] changed it
+bart_subset <- top_n(bart_data, 1000) 
 
 lotteries_theta %>%
   left_join(bart_subset, by = "partid") %>%
